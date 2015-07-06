70e157047ad3febd4e5ce03eeccc09fd36a79dca
shashank saxena <shashanksaxena18@gmail.com>
2015-07-06 16:40:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Endless Darkness</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="fonts/fonts.css" />
+	<link rel="shortcut icon" type="image/ico" href=""/>
+
+
+	<meta itemprop="name" content="Take a Look at This Short Stories ebook  Endless Darkness: 26 Hand Picked Horror Stories its free"/>
+<meta itemprop="description" content="Endless Darkness is a collection of 26 short horror stories showcasing the work of 19 authors. "/>
+    </head>
+    <body>
+  
+		<div>
+                <footer>
+    <li><a href="https://accounts.google.com/o/oauth2/auth?client_id=YOUR-CLIENT-ID&redirect_uri=REDIRECT-URI&scope=https://www.google.com/m8/feeds/&response_type=code" onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" >Import contact</a></li>
+
+          
+	</div>
+	
+    </body>
+    
+</html>
+
+
+
+
+
